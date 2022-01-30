b813af95ede20789915a7ab788c3d0fba13ea808
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-01-30 20:38:51 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+var checkButton = document.querySelector("#check-button");
+var message = document.querySelector("#error-message");
+var NoOfNotes = document.querySelectorAll(".no-of-notes");
+
+const NotesAvailable = [2000 , 500 , 100 , 20 , 10 , 5 , 1];
+
+// console.log(billAmount.value);
+checkButton.addEventListener("click" , function EvaluateBillAndCashAmount(){
+    hideMessage();
+    if(billAmount.value > 0){
+        var amountToBeReturned = cashGiven.value - billAmount.value;
+        if(amountToBeReturned > 0){
+            calculateAmount(amountToBeReturned);
+        }
+        else{
+           showMessage("No returns");
+        }
+        
+    }
+    else{
+       showMessage("Invalid Bill amount");
+    }
+});
+
+function showMessage(newMessage){
+    message.style.display = "block";
+    message.innerText = newMessage;
+}
+function calculateAmount(amountToBeReturned){
+    for(var i = 0 ; i < NotesAvailable.length ; i++){
+        var NumberOfNotes = Math.trunc(amountToBeReturned / NotesAvailable[i]);
+        amountToBeReturned %= NotesAvailable[i];
+
+        NoOfNotes[i].innerText = NumberOfNotes;
+    }
+}
+
+function hideMessage(){
+    message.style.display = "none";
+}
+
+function showSection(){
+    document.getElementById("next-container").style.display = "" ;
+}
